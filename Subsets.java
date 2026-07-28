b32c32191236ea3d8e9acde0ae01d4eb0eee3dff
vinay <vinaygadge2003@gmail.com>
2026-07-29 02:27:30 +0530

    Subsets

--- Subsets.java
+++ Subsets.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class Subsets {
-
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>>ans=new ArrayList<>();
+            List<Integer>l1=new ArrayList<>();
+            func(ans,l1,nums,0);
+            return ans;
+        }
+        public static void func(List<List<Integer>>ans,List<Integer>l1,int nums[],int i){
+            if(i>=nums.length){
+                ans.add(new ArrayList<>(l1));
+                return;
+            }
+            l1.add(nums[i]);
+            func(ans,l1,nums,i+1);
+            l1.remove(l1.size()-1);
+            func(ans,l1,nums,i+1);
+        }
+    }
 }
